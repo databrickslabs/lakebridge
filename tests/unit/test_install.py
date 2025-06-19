51cce675981ef3d2074050c6a0ed499289e2ca9a
Andrew Snare <andrew.snare@databricks.com>
2025-06-19 13:51:43 +0200

    Improve the way we prevent java from being found for the test.
    In particular, on modern Linux /bin and /usr/bin are the same so java can be found in both locations.

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -1330,21 +1330,22 @@ def test_store_product_state(tmp_path) -> None:
 @pytest.fixture
 def no_java(monkeypatch: pytest.MonkeyPatch) -> None:
     """Ensure that (temporarily) no 'java' binary can be found in the environment."""
-    existing_java = shutil.which("java")
-    if existing_java is None:
-        # Nothing to do: no Java binary found.
-        return
-    # Java is installed, so we need to figure out how to remove it from the path.
-    java_directory = Path(existing_java).parent
-    search_path = os.environ.get("PATH", os.defpath).split(os.pathsep)
-    modified_path = [p for p in search_path if p != str(java_directory)]
+    found_java = shutil.which("java")
+    while found_java is not None:
+        # Java is installed, so we need to figure out how to remove it from the path.
+        # (We loop here to handle cases where multiple java binaries are available via the PATH.)
+        java_directory = Path(found_java).parent
+        search_path = os.environ.get("PATH", os.defpath).split(os.pathsep)
+        updated_path = os.pathsep.join(p for p in search_path if p and Path(p) != java_directory)
+        assert (
+            search_path != updated_path
+        ), f"Did not find {java_directory} in {search_path}, but 'java' was found at {found_java}."
 
-    # Set the modified PATH without the directory where 'java' was found.
-    monkeypatch.setenv("PATH", os.pathsep.join(modified_path))
+        # Set the modified PATH without the directory where 'java' was found.
+        monkeypatch.setenv("PATH", os.pathsep.join(updated_path))
 
-    # Sanity check: ensure that 'java' is no longer found.
-    expected_missing = shutil.which("java")
-    assert expected_missing is None, "The 'java' executable should not be found in the modified PATH."
+        # Check again if 'java' is still found.
+        found_java = shutil.which("java")
 
 
 def test_java_version_with_java_missing(no_java: None) -> None:
