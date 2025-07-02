e0ddf691cf71713010ac9119e2146a28bc7ceed2
sundar.shankar <sundar.shankar@databricks.com>
2025-07-02 19:39:34 +0530

    Frame fitting

--- docs/lakebridge/src/components/ReconcileTabs.tsx
+++ docs/lakebridge/src/components/ReconcileTabs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef,useEffect} from 'react';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -18,7 +18,39 @@ const frameStyle: React.CSSProperties = {
 };
 
 const FrameTab: React.FC<FrameTabProps> = ({ src, label }) => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
     const url = useBaseUrl(src);
+
+    useEffect(() => {
+        const iframe = iframeRef.current;
+        if (!iframe) return;
+
+        const onLoad = () => {
+            try {
+                const doc = iframe.contentDocument || iframe.contentWindow?.document;
+                if (!doc) return;
+
+                const elementsToRemove = doc.querySelectorAll(
+                    'div[data-testid="content-spacer"], div[data-testid="extra-whitespace"]'
+                );
+
+                elementsToRemove.forEach(el => {
+                    if (!el.children.length) {
+                        el.remove();
+                    }
+                });
+            } catch (err) {
+                console.warn(err);
+            }
+        };
+
+        iframe.addEventListener('load', onLoad);
+
+        return () => {
+            iframe.removeEventListener('load', onLoad);
+        };
+    }, [url]);
+
     return (
         <div>
             <div style={{ marginBottom: '0.5em' }}>
@@ -26,14 +58,14 @@ const FrameTab: React.FC<FrameTabProps> = ({ src, label }) => {
                     Open notebook in new tab
                 </a>
             </div>
-            <iframe src={url} style={frameStyle} title={label} />
+            <iframe ref={iframeRef} src={url} style={frameStyle} title={label} />
         </div>
     );
 };
 
 const LakebridgeTabs: React.FC = () => (
     <Tabs>
-{/*        <TabItem value="Readme" label="Readme" default>
+        {/*        <TabItem value="Readme" label="Readme" default>
             <FrameTab src="/lakebridge_reconcile/Readme.html" label="Readme" />
         </TabItem>*/}
         <TabItem value="Recon Main" label="Recon Main">
