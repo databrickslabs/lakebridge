13778e34901e9ac436804065e0e291777b33f5c5
sundarshankar89 <sundar.shankar@databricks.com>
2025-10-08 10:39:54 +0530

    init commit

---
+++ tests/integration/helpers/test_metastore.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+from databricks.sdk import WorkspaceClient
+
+from databricks.labs.lakebridge.helpers.metastore import CatalogOperations
+from databricks.sdk.service.catalog import SecurableType, Privilege
+
+
+@pytest.fixture
+def catalog_ops(ws: WorkspaceClient):
+    return CatalogOperations(ws)
+
+
+# TODO: Add more tests for Resource Configurator, for now this helps when we upgrade databricks sdk
+@pytest.mark.skipif(not os.environ.get("CI"), reason="Runs only in CI")
+def test_has_privileges_user_has_all(ws: WorkspaceClient, catalog_ops: CatalogOperations):
+    user = ws.current_user.me().user_name
+    # Ensure the user has the required privileges in the test environment
+    assert catalog_ops.has_privileges(
+        user=user,
+        securable_type=SecurableType.CATALOG,
+        full_name="sandbox",
+        privileges={Privilege.ALL_PRIVILEGES},
+    )
