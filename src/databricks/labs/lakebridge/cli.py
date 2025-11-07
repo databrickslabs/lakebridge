19dc220380a14e418ee0ec2f71c2cf0a3554b879
Will Girten <will.girten@databricks.com>
2025-11-07 14:45:03 -0500

    Fix newline formatting in CLI.

--- src/databricks/labs/lakebridge/cli.py
+++ src/databricks/labs/lakebridge/cli.py
@@ -858,7 +858,8 @@ def execute_database_profiler(w: WorkspaceClient, source_tech: str | None = None
 
     # TODO: Add extractor logic to ApplicationContext instead of creating inside the Profiler class
     profiler.profile()
-    
+
+
 @lakebridge.command()
 def create_profiler_dashboard(
     *,
@@ -874,6 +875,7 @@ def create_profiler_dashboard(
     ctx.add_user_agent_extra("cmd", "create-profiler-dashboard")
     ctx.dashboard_manager.upload_duckdb_to_uc_volume(extract_file, volume_path)
     ctx.dashboard_manager.create_profiler_summary_dashboard(source_tech, catalog_name, schema_name)
+
 
 if __name__ == "__main__":
     app = lakebridge
