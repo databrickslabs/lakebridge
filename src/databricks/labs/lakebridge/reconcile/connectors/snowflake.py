4ae37c5e9e6a2133ac1b74bf0b91c5042439e622
M Abulazm <mohamed.abulazm@databricks.com>
2025-12-22 16:59:18 +0100

    fix snowflake auth handling

--- src/databricks/labs/lakebridge/reconcile/connectors/snowflake.py
+++ src/databricks/labs/lakebridge/reconcile/connectors/snowflake.py
@@ -65,14 +65,15 @@ class SnowflakeDataSource(DataSource, JDBCReaderMixin):
         raise RuntimeError("Snowflake credentials have not been loaded. Please call load_credentials() first.")
 
     def load_credentials(self, creds: ReconcileCredentialsConfig) -> "SnowflakeDataSource":
-        password = creds.vault_secret_names.pop("sfPassword", None)
-        pem_key = creds.vault_secret_names.pop("pem_private_key", None)
+        password = creds.vault_secret_names.get("sfPassword")
+        pem_key = creds.vault_secret_names.get("pem_private_key")
         if password and pem_key:  # user did not specify auth method after migrating from secret scope
             logger.warning(
                 f"Snowflake auth not specified after migrating from secret scope so defaulting to sfPassword. "
                 f"Please update the creds config and include the necessary keys. Docs: {self._DOCS_URL}."
             )
-            creds.vault_secret_names["sfPassword"] = password
+            creds.vault_secret_names.pop("pem_private_key")
+
         self._creds_or_empty = load_and_validate_credentials(creds, self._ws, "snowflake")
 
         # Ensure at least one authentication method is provided
