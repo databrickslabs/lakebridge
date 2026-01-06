81784f25df24c3eda7ac1d5f7ac6eba194cd0728
Andrew Snare <andrew.snare@databricks.com>
2026-01-06 16:10:48 +0100

    Stop a few random places in the code from blindly overwriting initialized log-levels.

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -1640,8 +1640,6 @@ def test_no_reconfigure_if_noninteractive(
         ),
     )
 
-    logging.getLogger("databricks.labs.lakebridge").setLevel(logging.DEBUG)
-
     installer = ws_installer(
         ctx.workspace_client,
         ctx.prompts,
