dc51eb684c5f1b4bc9d241be811b8acee69f80ee
M Abulazm <mohamed.abulazm@databricks.com>
2026-01-13 16:51:23 +0100

    Revert "add select_columns"
    This reverts commit 6c3155d9404a0206efd41f13299a32aafe0b57cf.

--- tests/integration/reconcile/test_recon_databricks.py
+++ tests/integration/reconcile/test_recon_databricks.py
@@ -37,7 +37,6 @@ def recon_table_config(recon_schema: SchemaInfo, recon_tables: tuple[TableInfo, 
             Table(
                 source_name=src_table.name,
                 target_name=tgt_table.name,
-                select_columns=["carat", "cut", "color", "clarity"],
                 join_columns=["color", "clarity"],
             )
         ]
