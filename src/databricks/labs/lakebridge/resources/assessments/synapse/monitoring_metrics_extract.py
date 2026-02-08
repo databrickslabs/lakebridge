1d241c9567331e07af01631ba888c46365110bec
sundarshankar89 <sundar.shankar@databricks.com>
2026-02-08 20:30:42 +0530

    modified constructor args to match create_credential_manager

--- src/databricks/labs/lakebridge/resources/assessments/synapse/monitoring_metrics_extract.py
+++ src/databricks/labs/lakebridge/resources/assessments/synapse/monitoring_metrics_extract.py
@@ -9,6 +9,7 @@ from databricks.labs.blueprint.entrypoint import get_logger
 from databricks.labs.lakebridge import initialize_logging
 from databricks.labs.lakebridge.assessments import PRODUCT_NAME
 from databricks.labs.lakebridge.connections.credential_manager import create_credential_manager
+from databricks.labs.lakebridge.connections.env_getter import EnvGetter
 from databricks.labs.lakebridge.resources.assessments.synapse.common.duckdb_helpers import insert_df_to_duckdb
 from databricks.labs.lakebridge.resources.assessments.synapse.common.functions import (
     arguments_loader,
@@ -25,7 +26,7 @@ logger = get_logger(__file__)
 
 def execute():
     db_path, creds_file = arguments_loader(desc="Monitoring Metrics Extract Script")
-    cred_manager = create_credential_manager(PRODUCT_NAME, creds_file)
+    cred_manager = create_credential_manager(PRODUCT_NAME, EnvGetter())
     synapse_workspace_settings = cred_manager.get_credentials("synapse")
     synapse_profiler_settings = synapse_workspace_settings["profiler"]
 
