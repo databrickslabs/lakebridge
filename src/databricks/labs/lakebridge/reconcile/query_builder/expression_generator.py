de1a08726af9cb7756483033b7456a4c8b131a25
M Abulazm <mohamed.abulazm@databricks.com>
2026-02-23 14:25:34 +0100

    add default transform for tsql

--- src/databricks/labs/lakebridge/reconcile/query_builder/expression_generator.py
+++ src/databricks/labs/lakebridge/reconcile/query_builder/expression_generator.py
@@ -264,6 +264,7 @@ DataType_transform_mapping: dict[str, dict[str, list[partial[exp.Expression]]]] 
         ],
     },
     "tsql": {
+        "default": [partial(anonymous, func="COALESCE(TRIM(CAST({} AS VARCHAR(MAX))), '_null_recon_')")],
         exp.DataType.Type.DATE.value: [partial(anonymous, func="COALESCE(CONVERT(DATE, {0}, 101), '1900-01-01')")],
         exp.DataType.Type.TIME.value: [partial(anonymous, func="COALESCE(CONVERT(TIME, {0}, 108), '00:00:00')")],
         exp.DataType.Type.DATETIME.value: [
