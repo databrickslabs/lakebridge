5592abca40e18d95ea198713e2cb9f908f884b5d
M Abulazm <mohamed.abulazm@databricks.com>
2026-05-08 12:30:50 +0200

    fix outdated usages of mock_spark

--- tests/integration/reconcile/connectors/test_read_schema.py
+++ tests/integration/reconcile/connectors/test_read_schema.py
@@ -75,36 +75,36 @@ def test_snowflake_read_schema_happy(spark: SparkSession) -> None:
     assert columns
 
 
-def test_sql_server_list_schemas_happy(mock_spark: SparkSession) -> None:
+def test_sql_server_list_schemas_happy(spark: SparkSession) -> None:
     connection = TestEnvGetter(False).get("TEST_TSQL_CONNECTION")
-    reader = RemoteQueryReader(mock_spark, connection)
+    reader = RemoteQueryReader(spark, connection)
     connector = TSQLServerDataSource(get_dialect("tsql"), reader)
 
     schemas = connector.list_schemas("labs_azure_sandbox_remorph")
     assert "dbo" in schemas
 
 
-def test_sql_server_list_tables_happy(mock_spark: SparkSession) -> None:
+def test_sql_server_list_tables_happy(spark: SparkSession) -> None:
     connection = TestEnvGetter(False).get("TEST_TSQL_CONNECTION")
-    reader = RemoteQueryReader(mock_spark, connection)
+    reader = RemoteQueryReader(spark, connection)
     connector = TSQLServerDataSource(get_dialect("tsql"), reader)
 
     tables = connector.list_tables("labs_azure_sandbox_remorph", "dbo")
     assert "reconcile_in" in tables
 
 
-def test_snowflake_list_schemas_happy(mock_spark: SparkSession) -> None:
+def test_snowflake_list_schemas_happy(spark: SparkSession) -> None:
     connection = TestEnvGetter(False).get("TEST_SNOWFLAKE_CONNECTION")
-    reader = RemoteQueryReader(mock_spark, connection)
+    reader = RemoteQueryReader(spark, connection)
     connector = SnowflakeDataSource(get_dialect("snowflake"), reader)
 
     schemas = connector.list_schemas("remorph")
     assert "SANDBOX" in schemas
 
 
-def test_snowflake_list_tables_happy(mock_spark: SparkSession) -> None:
+def test_snowflake_list_tables_happy(spark: SparkSession) -> None:
     connection = TestEnvGetter(False).get("TEST_SNOWFLAKE_CONNECTION")
-    reader = RemoteQueryReader(mock_spark, connection)
+    reader = RemoteQueryReader(spark, connection)
     connector = SnowflakeDataSource(get_dialect("snowflake"), reader)
 
     tables = connector.list_tables("remorph", "sandbox")
@@ -139,16 +139,16 @@ def test_databricks_list_tables_happy_sandbox(
 
 
 @pytest.mark.skip(reason="Not Ready! Deploy Infra")
-def test_oracle_list_schemas_happy(mock_spark: SparkSession) -> None:
-    connector = OracleDataSourceUnderTest(mock_spark)
+def test_oracle_list_schemas_happy(spark: SparkSession) -> None:
+    connector = OracleDataSourceUnderTest(spark)
 
     schemas = connector.list_schemas("ORCL")
     assert "SYSTEM" in schemas
 
 
 @pytest.mark.skip(reason="Not Ready! Deploy Infra")
-def test_oracle_list_tables_happy(mock_spark: SparkSession) -> None:
-    connector = OracleDataSourceUnderTest(mock_spark)
+def test_oracle_list_tables_happy(spark: SparkSession) -> None:
+    connector = OracleDataSourceUnderTest(spark)
 
     tables = connector.list_tables("ORCL", "SYSTEM")
     assert tables
